4e891ae778b2b974825a11385f0930c0aea87c86
James McFeeters <jameeters@gmail.com>
2023-01-03 14:46:51 -0600

    Working but very messy PoC

--- src/main/java/org/pankratzlab/internal/gwas/MatchingVariable.java
+++ src/main/java/org/pankratzlab/internal/gwas/MatchingVariable.java
@@ -1,5 +1,10 @@
 package org.pankratzlab.internal.gwas;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.pankratzlab.common.ext;
 
 public class MatchingVariable {
@@ -8,6 +13,25 @@ public class MatchingVariable {
   final boolean isBinary;
 
   private int headerIndex = -1;
+
+  // for continuous variables
+  private int casesSeen = 0;
+  private int controlsSeen = 0;
+
+  private double caseSum = 0;
+  private double controlSum = 0;
+
+  private double caseAvg;
+  private double controlAvg;
+
+  // for binary variables
+  Map<String, Integer> caseValues = new HashMap<>();
+  private Map<String, List<Integer>> previousControlValues = new HashMap<>();
+
+  private int totalSeen = 0;
+  private int matchesSeen = 0;
+
+  private double concordance;
 
   public MatchingVariable(String headerName, boolean isBinary) {
     this.headerName = headerName;
@@ -22,5 +46,58 @@ public class MatchingVariable {
 
   public int getHeaderIndex() {
     return headerIndex;
+  }
+
+  public void addToCaseSum(double value) {
+    caseSum += value;
+    casesSeen += 1;
+  }
+
+  public void addToControlSum(double value) {
+    controlSum += value;
+    controlsSeen += 1;
+  }
+
+  public void setCaseValue(String caseId, int value) {
+    caseValues.put(caseId, value);
+    if (previousControlValues.containsKey(caseId)) {
+      for (int v : previousControlValues.get(caseId)) {
+        if (v == value) {
+          matchesSeen += 1;
+        }
+        totalSeen += 1;
+      }
+    }
+    previousControlValues.remove(caseId);
+  }
+
+  public void recordControlValue(String caseId, int value) {
+    if (caseValues.containsKey(caseId)) {
+      if (value == caseValues.get(caseId)) {
+        matchesSeen += 1;
+      }
+      totalSeen += 1;
+    } else {
+      if (previousControlValues.containsKey(caseId)) {
+        previousControlValues.get(caseId).add(value);
+      } else {
+        previousControlValues.put(caseId, new ArrayList<>(List.of(value)));
+      }
+    }
+  }
+
+  public double getCaseAvg() {
+    this.caseAvg = caseSum / casesSeen;
+    return caseAvg;
+  }
+
+  public double getControlAvg() {
+    controlAvg = controlSum / controlsSeen;
+    return controlAvg;
+  }
+
+  public double getConcordance() {
+    concordance = (double) matchesSeen / (double) totalSeen;
+    return concordance;
   }
 }
--- src/main/java/org/pankratzlab/internal/gwas/REval.java
+++ src/main/java/org/pankratzlab/internal/gwas/REval.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.pankratzlab.common.Files;
 import org.pankratzlab.common.PSF;
@@ -13,7 +15,9 @@ public class REval {
   final MatchingVariable[] matchingVariables;
   final File statusFile;
   final File phenotypeFile;
-  final Map<String, String> pairings;
+  final Map<String, String> controlCasePairings;
+  final Set<String> cases;
+  final Set<String> controls;
 
   public REval(MatchingVariable[] matchingVariables, File statusFile,
                File phenotypeFile) throws IOException {
@@ -25,21 +29,50 @@ public class REval {
     this.statusFile = statusFile;
     this.phenotypeFile = phenotypeFile;
 
-    this.pairings = readPairings();
+    this.controlCasePairings = readPairings();
+    this.controls = controlCasePairings.keySet();
+    this.cases = new HashSet<>(controlCasePairings.values());
   }
 
   public void evaluate() throws IOException {
     BufferedReader phenoReader = Files.getAppropriateReader(phenotypeFile.toString());
-    String[] phenoHeader = phenoReader.readLine().trim().split(PSF.Regex.GREEDY_WHITESPACE);
+    String[] phenoHeader = splitTsvLine(phenoReader.readLine());
 
+    // figure out which column corresponds to each matching variable
     Map<Integer, MatchingVariable> matchingVarIndexMap = new HashMap<>();
     for (MatchingVariable mv : matchingVariables) {
       int i = mv.findIndexInHeader(phenoHeader);
-      assert i != -1; // todo: check that all the matching variables end up in the map
+      assert i != -1; // todo: checks that all the matching variables end up in the map
       matchingVarIndexMap.put(i, mv);
       // todo: there should be some check that no two matching variables share an index
     }
 
+    String[] line;
+    String inputLine = phenoReader.readLine();
+    while (inputLine != null) {
+      line = splitTsvLine(inputLine);
+      String sampleId = line[0];
+      if (cases.contains(sampleId) || controls.contains(sampleId)) {
+        for (int i : matchingVarIndexMap.keySet()) {
+          MatchingVariable mv = matchingVarIndexMap.get(i);
+          if (mv.isBinary) {
+            if (cases.contains(sampleId)) {
+              mv.setCaseValue(sampleId, Integer.parseInt(line[i]));
+            } else {
+              mv.recordControlValue(controlCasePairings.get(sampleId), Integer.parseInt(line[i]));
+            }
+          } else {
+            double value = Double.parseDouble(line[i]);
+            if (cases.contains(sampleId)) {
+              mv.addToCaseSum(value);
+            } else {
+              mv.addToControlSum(value);
+            }
+          }
+        }
+      }
+      inputLine = phenoReader.readLine();
+    }
   }
 
   private Map<String, String> readPairings() throws IOException {
@@ -55,7 +88,7 @@ public class REval {
     String[] line;
     String inputLine = reader.readLine();
     while (inputLine != null) {
-      line = inputLine.trim().split(PSF.Regex.GREEDY_WHITESPACE);
+      line = splitTsvLine(inputLine);
       // We don't want any (case -> itself) pairs
       // we don't care about a case's relationship to itself
       boolean isNotCase = Integer.parseInt(line[1]) != 1;
@@ -65,5 +98,9 @@ public class REval {
       inputLine = reader.readLine();
     }
     return pairings;
+  }
+
+  private static String[] splitTsvLine(String inputLine) {
+    return inputLine.trim().split(PSF.Regex.GREEDY_WHITESPACE);
   }
 }
