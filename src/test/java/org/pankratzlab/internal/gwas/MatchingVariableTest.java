39f77ba8d61eba900a92cea4092cb15ddd6475d9
James McFeeters <jameeters@gmail.com>
2023-01-05 11:49:58 -0600

    Add a basic test for REval

--- src/test/java/org/pankratzlab/internal/gwas/MatchingVariableTest.java
+++ src/test/java/org/pankratzlab/internal/gwas/MatchingVariableTest.java
@@ -1,6 +1,8 @@
 package org.pankratzlab.internal.gwas;
 
+
 import org.junit.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -39,4 +41,26 @@ public class MatchingVariableTest {
 
     assertEquals(0.5, mv.getConcordance());
   }
+
+  @Test
+  public void testMethodRestrictionBasedOnBinary() {
+    MatchingVariable binary = new MatchingVariable("foo", true);
+    Executable[] nonBinaryOnlyMethods = new Executable[] {() -> binary.addToControlSum(0.2),
+                                                          () -> binary.addToCaseSum(0.3),
+                                                          binary::getControlAvg,
+                                                          binary::getCaseAvg};
+    for (Executable method : nonBinaryOnlyMethods) {
+      assertThrows(UnsupportedOperationException.class, method);
+    }
+
+    MatchingVariable continuous = new MatchingVariable("bar", false);
+    Executable[] binaryOnlyMethods = new Executable[] {() -> continuous.recordControlValue("case",
+                                                                                           1),
+                                                       () -> continuous.setCaseValue("case", 0),
+                                                       continuous::getConcordance};
+    for (Executable method : binaryOnlyMethods) {
+      assertThrows(UnsupportedOperationException.class, method);
+    }
+  }
+
 }
---
+++ src/test/java/org/pankratzlab/internal/gwas/REvalTest.java
@@ -0,0 +1,26 @@
+package org.pankratzlab.internal.gwas;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class REvalTest {
+
+  @Test
+  public void basicFunctionTest() throws IOException {
+    MatchingVariable foo = new MatchingVariable("foo", false);
+    MatchingVariable bar = new MatchingVariable("bar", true);
+    File status = new File("src/test/resources/status.tsv");
+    File phenotype = new File("src/test/resources/phenotype.tsv");
+
+    REval rEval = new REval(new MatchingVariable[] {foo, bar}, status, phenotype);
+    rEval.evaluate();
+
+    assertEquals(0.0, foo.getCaseAvg());
+    assertEquals(0.0, foo.getControlAvg());
+    assertEquals(0.5, bar.getConcordance());
+  }
+}
